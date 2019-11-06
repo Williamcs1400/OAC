9bad015e8669af77461742dd475178a6f507895d
William Coelho da Silva <39714540+Williamcs1400@users.noreply.github.com>
2019-11-05 21:02:51 -0300

    Add files via upload

--- Trabalho 2 - OAC/p_ula.vhd
+++ Trabalho 2 - OAC/p_ula.vhd
@@ -14,12 +14,23 @@ end entity p_ula;
 
 architecture p_ula of p_ula is
 begin 
-	result <= std_logic_vector(signed(a) + signed(b)) when op = "0000" else 	-- Soma de a e b
-	          std_logic_vector(signed(a) - signed(b)) when op = "0001" else		-- Subtracao de a e b
-				 to_stdlogicvector(to_bitvector(a) sll to_integer(unsigned(b))) when op = "0010" else		-- Desloca a b bits a esquerda 
+	result <= std_logic_vector(signed(a) + signed(b)) when op = "0000" else 	-- Soma de a e b (add)
+	          std_logic_vector(signed(a) - signed(b)) when op = "0001" else		-- Subtracao de a e b (sub)
 				 
-				 std_logic_vector(signed(a) or signed(b)) when op = "1000" else	-- Ou logico bit a bit entre a e b
-				 std_logic_vector(signed(a) and signed(b)) when op = "1001" else	-- E logico bit a bit entre a e b
-				 a;
+				 to_stdlogicvector(to_bitvector(a) sll to_integer(unsigned(b))) when op = "0010" else		-- Desloca a b bits a esquerda (sll)
+				 to_stdlogicvector(to_bitvector(a) srl to_integer(unsigned(b))) when op = "0110" else 		-- Desloca a b bits a direita sem manter o sinal (srl)
+				 to_stdlogicvector(to_bitvector(a) sra to_integer(unsigned(b))) when op = "0111" else 		-- Desloca a b bits a direita mantendo o sinal (sra)
+				 
+				 x"00000001" when op = "0011" and signed(a) < signed(b) else 		-- Joga 1 na saida se a for menor que b levando em conta o sinal (slt)
+				 x"00000001" when op = "0100" and unsigned(a) < unsigned(b) else 	-- Joga 1 na saida se a for menor que b sem levar em conta o sinal (sltu)
+				 
+				 std_logic_vector(signed(a) xor signed(b)) when op ="0101" else	-- XOR logico bit a bit entre a e b (xor)
+				 std_logic_vector(signed(a) or signed(b)) when op = "1000" else	-- OU logico bit a bit entre a e b (ou)
+				 std_logic_vector(signed(a) and signed(b)) when op = "1001" else	-- E logico bit a bit entre a e b (and)
+				 
+				 x"00000001" when op = "1010" and signed(a) = signed(b) else 		-- Joga 1 na saida se a for igual a b (seq)
+				 x"00000001" when op = "1011" and signed(a) /= signed(b) else 		-- Joga 1 ma saida se a for diferente b (sne)
+				 x"00000001" when op = "1100" and signed(a) >= signed(b) else 		-- Joga 1 ma saida se a for maior igual que b co sinal (sge)
+				 x"00000001" when op = "1101" and unsigned(a) >= unsigned(b);	 	-- Joga 1 ma saida se a for maior igual que b co sinal (sgeu)
 				 
 end p_ula;
