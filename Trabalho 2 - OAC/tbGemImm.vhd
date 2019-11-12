92a372d98ccb67bbfbf7599db6514a49629d8990
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2019-11-12 10:54:54 -0300

    Update tbGemImm.vhd

--- Trabalho 2 - OAC/tbGemImm.vhd
+++ Trabalho 2 - OAC/tbGemImm.vhd
@@ -10,15 +10,13 @@ end tbGemImm;
 architecture tbGemImm_arch of tbGemImm is
    signal tbGemImm_inst : std_logic_vector(31 downto 0);
 	signal tbGemImm_imm32 : signed(31 downto 0);
-	--signal tbGemImm_opcode : std_logic_vector(6 downto 0);
+
 begin
 
      	DUT : entity work.gemImm
 			  port map (inst => tbGemImm_inst, imm32 => tbGemImm_imm32);
 
-	tbGemImm_inst <= x"000002B3", x"01002283" after 30 ns, x"F9C00313" after 60 ns, x"FFF2C293" after 90 ns, x"01800067" after 120 ns, x"00002437" after 150 ns, x"02542E23" after 180 ns, x"FE5290E3" after 210 ns, x"00C000EF" after 240 ns;
-
-	--tbGemImm_inst <= x"000002B3", x"01002283" after 10 ns,  -- 0 ... 1
+	tbGemImm_inst <= x"000002B3", x"01002283" after 30 ns, x"F9C00313" after 60 ns, x"FFF2C293" after 90 ns, x"16200313" after 120 ns, x"01800067" after 150 ns, x"00002437" after 180 ns, x"02542E23" after 210 ns, x"FE5290E3" after 240 ns, x"00C000EF" after 270 ns;
 
 
 end tbGemImm_arch;
